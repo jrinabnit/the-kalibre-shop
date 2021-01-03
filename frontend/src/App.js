96473e85f48aaaa36da0590250e30257ccb5bd60
jrinabnit <jrinabnit@gmail.com>
2021-01-03 17:42:57 +0800

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
-const App = ()=> {
-  return (
-   <>
-   <h1>Welcome to the Shop</h1>
-   </>
-  );
-}
+const App = () => {
+	return (
+		<Router>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<Route path='/login' component={LoginScreen} />
+					<Route path='/register' component={RegisterScreen} />
+					<Route path='/product/:id' component={ProductScreen} />
+					<Route path='/cart/:id?' component={CartScreen} />
+					<Route path='/' component={HomeScreen} exact />
+				</Container>
+			</main>
+			<Footer />
+		</Router>
+	);
+};
 
 export default App;
