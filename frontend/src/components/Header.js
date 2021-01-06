d4373c675fca650451cbe461960e3cb218c1fc76
jrinabnit <jrinabnit@gmail.com>
2021-01-06 12:15:25 +0800

    adding profile screen and updating user actions, reducers, and constants

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -20,14 +20,24 @@ const Header = () => {
 					<LinkContainer to='/'>
 						<Navbar.Brand>The Gear Shop</Navbar.Brand>
 					</LinkContainer>
+					
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					
 					<Navbar.Collapse id='basic-navbar-nav'>
+						
 						<Nav className='ml-auto'>
+							
+							
+							
 							<LinkContainer to='/cart'>
 								<Nav.Link href='/cart'>
 									<i className='fas fa-shopping-cart' /> Cart
 								</Nav.Link>
 							</LinkContainer>
+							
+							
+							
+							
 							{userInfo ? (
 								<NavDropdown title={userInfo.name} id='username'>
 									<LinkContainer to='/profile'>
@@ -43,6 +53,8 @@ const Header = () => {
 								</LinkContainer>
 							)}
 						</Nav>
+					
+					
 					</Navbar.Collapse>
 				</Container>
 			</Navbar>
