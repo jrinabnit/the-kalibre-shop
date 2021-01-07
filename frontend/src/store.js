d8f25a33d3b3e8fc8763da9703855b349da6b433
jrinabnit <jrinabnit@gmail.com>
2021-01-07 15:15:48 +0800

    adding checkout steps and order screens, constants, actions, and reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,6 +9,7 @@ import {
 	userRegisterReducer,
 	userUpdateProfileReducer,
 } from './reducers/userReducers';
+import { orderCreateReducer } from './reducers/orderReducers';
 
 const reducer = combineReducers({
 	productList: productListReducer,
@@ -18,14 +19,19 @@ const reducer = combineReducers({
 	userRegister: userRegisterReducer,
 	userDetails: userDetailsReducer,
 	userUpdateProfile: userUpdateProfileReducer,
+	orderCreate: orderCreateReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
 
 const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+	? JSON.parse(localStorage.getItem('shippingAddress'))
+	: {};
+
 const initialState = {
-	cart: { cartItems: cartItemsFromStorage },
+	cart: { cartItems: cartItemsFromStorage, shippingAddress: shippingAddressFromStorage },
 	userLogin: { userInfo: userInfoFromStorage },
 };
 
